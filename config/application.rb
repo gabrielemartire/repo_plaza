9de5437172be6afe152e4047a75d4a396b3d07e1
gabrielemartire <gabriele.martire@gmail.com>
2025-08-31 12:36:02 +0200

    fixing deploy on railway

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,7 @@ module RepoPlaza
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
     
-    config.require_master_key = true
+    config.require_master_key = false
 
     # Configuration for the application, engines, and railties goes here.
     #
